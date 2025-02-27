60729185def3f0669a539f2eaa47a8373eff3f5a
Pavel Tikhonin <p.tikhonin@mail.ru>
2025-02-27 10:58:22 +0300

    fix: enhance password handling logging

--- server/models/User.js
+++ server/models/User.js
@@ -50,32 +50,62 @@ const UserSchema = new mongoose.Schema({
 
 // Encrypt password using bcrypt
 UserSchema.pre('save', async function(next) {
+  console.log('\n=== Password Encryption Hook ===');
+  console.log('1. Hook triggered for user:', this.username);
+  console.log('2. Password modified:', this.isModified('password'));
+  
   if (!this.isModified('password')) {
-    next();
+    console.log('3. Password not modified, skipping encryption');
+    return next();
   }
   
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
+  try {
+    console.log('4. Generating salt...');
+    const salt = await bcrypt.genSalt(10);
+    console.log('5. Hashing password...');
+    this.password = await bcrypt.hash(this.password, salt);
+    console.log('6. Password hashed successfully');
+    next();
+  } catch (error) {
+    console.error('Password encryption error:', error);
+    next(error);
+  }
 });
 
 // Match user entered password to hashed password in database
 UserSchema.methods.matchPassword = async function(enteredPassword) {
   console.log('\n=== Password Match Debug ===');
-  console.log('Attempting to match password...');
+  console.log('1. Attempting to match password for user:', this.username);
   
-  // Make sure we have the password field
-  const user = await this.model('User').findOne({ _id: this._id }).select('+password');
-  
-  if (!user || !user.password) {
-    console.log('No user or password found');
+  try {
+    // Make sure we have the password field
+    console.log('2. Fetching user with password field...');
+    const user = await this.model('User').findOne({ _id: this._id }).select('+password');
+    
+    if (!user || !user.password) {
+      console.log('3. No user or password found');
+      return false;
+    }
+
+    console.log('4. Found user with password:', {
+      hasUser: !!user,
+      username: user.username,
+      hasPassword: !!user.password,
+      passwordLength: user.password?.length
+    });
+
+    console.log('5. Attempting bcrypt compare...');
+    console.log('   Entered password length:', enteredPassword?.length);
+    console.log('   Stored password length:', user.password?.length);
+    
+    const isMatch = await bcrypt.compare(enteredPassword, user.password);
+    console.log('6. Password match result:', isMatch);
+
+    return isMatch;
+  } catch (error) {
+    console.error('Password match error:', error);
     return false;
   }
-
-  console.log('Found user with password, attempting bcrypt compare...');
-  const isMatch = await bcrypt.compare(enteredPassword, user.password);
-  console.log('Password match result:', isMatch);
-
-  return isMatch;
 };
 
 module.exports = mongoose.model('User', UserSchema); 
