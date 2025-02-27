f23e10600d0c0995eee52d4d1036ffa17325b47f
Pavel Tikhonin <p.tikhonin@mail.ru>
2025-02-27 13:09:41 +0300

    Prepare backend for deployment

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://localhost:5000'],
+  credentials: true
+}));
 app.use(express.json());
 
 // Route files
@@ -27,7 +30,7 @@ app.use('/api/books', bookRoutes);
 // Connect to database
 connectDB();
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
